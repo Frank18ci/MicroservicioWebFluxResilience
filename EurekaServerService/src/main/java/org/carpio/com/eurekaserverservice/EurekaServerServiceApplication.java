42dbf792365a6a0312f3fbfd9c315dba862c426d
Frank <fcarpio113@gmail.com>
2025-08-31 08:52:15 -0500

    Agregando mas services, eureka, gateway

---
+++ EurekaServerService/src/main/java/org/carpio/com/eurekaserverservice/EurekaServerServiceApplication.java
@@ -0,0 +1,15 @@
+package org.carpio.com.eurekaserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerServiceApplication.class, args);
+    }
+
+}
---
+++ productoService/src/test/java/org/carpio/com/productoservice/ProductoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carpio.com.productoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
